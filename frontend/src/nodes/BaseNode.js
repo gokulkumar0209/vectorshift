9e99c3d1b891ad6fc764b446bd49553b2dcbc318
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-09-16 16:11:08 +0530

    Node delete functionality added

--- frontend/src/nodes/BaseNode.js
+++ frontend/src/nodes/BaseNode.js
@@ -1,23 +1,41 @@
 import { Handle, Position } from "reactflow";
 import { useState } from "react";
-
+import { useStore } from "../store";
+import { shallow } from "zustand/shallow";
+const selector = (state) => ({
+	nodes: state.nodes,
+	edges: state.edges,
+	getNodeID: state.getNodeID,
+	addNode: state.addNode,
+	onNodesChange: state.onNodesChange,
+	onEdgesChange: state.onEdgesChange,
+	onConnect: state.onConnect,
+	onNodesDelete: state.onNodesDelete,
+});
 const BaseNode = ({ id, data, label, inputs, handles, className }) => {
 	const [currName, setCurrName] = useState("");
-
+	const {
+		nodes,
+		edges,
+		getNodeID,
+		addNode,
+		onNodesChange,
+		onEdgesChange,
+		onConnect,
+		onNodesDelete,
+	} = useStore(selector, shallow);
 	const handleChange = (e) => {
 		setCurrName(e.target.value);
 	};
-
+	const deleteNode = (e) => {
+		e.preventDefault();
+		onNodesDelete(id);
+	};
 	return (
-		<div
-			className={`${className}  rounded-lg shadow-lg p-4 `}
-		>
+		<div className={`${className}  rounded-lg shadow-lg p-4 `}>
 			<div className="font-bold text-center mb-2 flex justify-between items-center">
 				<span>{label}</span>
-				<h3
-					className="text-red-600 cursor-pointer"
-					onClick={() => console.log("Close button clicked")}
-				>
+				<h3 className="text-red-600 cursor-pointer" onClick={deleteNode}>
 					x
 				</h3>
 			</div>
@@ -72,7 +90,6 @@ const BaseNode = ({ id, data, label, inputs, handles, className }) => {
 					) : (
 						<>
 							<Handle
-							
 								type={type}
 								position={Position.Right}
 								id={`${id}-${idSuffix}`}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,53 +2,89 @@
 
 import { create } from "zustand";
 import {
-    addEdge,
-    applyNodeChanges,
-    applyEdgeChanges,
-    MarkerType,
-  } from 'reactflow';
+	addEdge,
+	applyNodeChanges,
+	applyEdgeChanges,
+	MarkerType,
+	getConnectedEdges,
+	getIncomers,
+	getOutgoers,
+} from "reactflow";
 
 export const useStore = create((set, get) => ({
-    nodes: [],
-    edges: [],
-    getNodeID: (type) => {
-        const newIDs = {...get().nodeIDs};
-        if (newIDs[type] === undefined) {
-            newIDs[type] = 0;
-        }
-        newIDs[type] += 1;
-        set({nodeIDs: newIDs});
-        return `${type}-${newIDs[type]}`;
-    },
-    addNode: (node) => {
-        set({
-            nodes: [...get().nodes, node]
-        });
-    },
-    onNodesChange: (changes) => {
-      set({
-        nodes: applyNodeChanges(changes, get().nodes),
-      });
-    },
-    onEdgesChange: (changes) => {
-      set({
-        edges: applyEdgeChanges(changes, get().edges),
-      });
-    },
-    onConnect: (connection) => {
-      set({
-        edges: addEdge({...connection, type: 'smoothstep', animated: true, markerEnd: {type: MarkerType.Arrow, height: '20px', width: '20px'}}, get().edges),
-      });
-    },
-    updateNodeField: (nodeId, fieldName, fieldValue) => {
-      set({
-        nodes: get().nodes.map((node) => {
-          if (node.id === nodeId) {
-            node.data = { ...node.data, [fieldName]: fieldValue };
-          }
-  
-          return node;
-        }),
-      });
-    },
-  }));
+	nodes: [],
+	edges: [],
+	getNodeID: (type) => {
+		const newIDs = { ...get().nodeIDs };
+		if (newIDs[type] === undefined) {
+			newIDs[type] = 0;
+		}
+		newIDs[type] += 1;
+		set({ nodeIDs: newIDs });
+		return `${type}-${newIDs[type]}`;
+	},
+	addNode: (node) => {
+		set({
+			nodes: [...get().nodes, node],
+		});
+	},
+	onNodesChange: (changes) => {
+		set({
+			nodes: applyNodeChanges(changes, get().nodes),
+		});
+	},
+	onEdgesChange: (changes) => {
+		set({
+			edges: applyEdgeChanges(changes, get().edges),
+		});
+	},
+	onConnect: (connection) => {
+		set({
+			edges: addEdge(
+				{
+					...connection,
+					type: "smoothstep",
+					animated: true,
+					markerEnd: { type: MarkerType.Arrow, height: "20px", width: "20px" },
+				},
+				get().edges
+			),
+		});
+	},
+	updateNodeField: (nodeId, fieldName, fieldValue) => {
+		set({
+			nodes: get().nodes.map((node) => {
+				if (node.id === nodeId) {
+					node.data = { ...node.data, [fieldName]: fieldValue };
+				}
+
+				return node;
+			}),
+		});
+	},
+	onNodesDelete: (idToDelete) => {
+		const { nodes, edges } = get();
+
+		// Filter out the node to be deleted
+		const remainingNodes = nodes.filter((node) => node.id !== idToDelete);
+
+		// Get the node to be deleted
+		const nodeToDelete = nodes.find((node) => node.id === idToDelete);
+
+		if (!nodeToDelete) return; // If node not found, exit
+
+		// Get connected edges
+		const connectedEdges = getConnectedEdges([nodeToDelete], edges);
+
+		// Remove connected edges
+		const remainingEdges = edges.filter(
+			(edge) => !connectedEdges.includes(edge)
+		);
+
+		// Update the state with remaining nodes and edges
+		set({
+			nodes: remainingNodes,
+			edges: remainingEdges,
+		});
+	},
+}));
